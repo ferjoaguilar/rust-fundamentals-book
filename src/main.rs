41ef92989eef1399cd88e7de91ccdb797b357d17
Fernando Aguilar <feraguilar6985@gmail.com>
2023-05-09 17:56:17 +0000

    add module to import

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-mod english;
-
-/* use english::greetings;
-use english::goodbyes; */
-
-//use english::{greetings, goodbyes};
-use english::{greetings as saludos, goodbyes as despedidas};
-
-
 fn main() {
-    println!("English func {}", saludos::hello());
-    println!("English func {}", despedidas::goodbye());
+    println!("Hello, world!")
 }
